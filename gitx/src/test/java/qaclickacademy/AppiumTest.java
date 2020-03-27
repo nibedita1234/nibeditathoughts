43cb1a73c75c8f18f0f72030e3efd9da908cf0d9
nibedita1234 <62743443+nibedita1234@users.noreply.github.com>
2020-03-27 20:02:38 +0530

    Add files via upload

---
+++ gitx/src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NetiveApp(){
+		System.out.println("NetiveApp");
+		
+	}
+	@Test
+	public void IosApp(){
+		System.out.println("IosApp");
+		
+	}
+	
+
+}
---
+++ gitx/src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void Postjira() {
+		System.out.println("postjira");
+	}
+	@Test
+	public void DeleteTwitter() {
+		System.out.println("DeleteTwitter");
+	}
+
+}
